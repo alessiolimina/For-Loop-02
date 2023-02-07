8865ff5dec81d23cb390488dae22a5229011ff21
Alessio Limina <alessio.limina@gmail.com>
2023-02-07 17:35:45 +0100

    Exercise For Loop 02

---
+++ src/it/develhope/forloop/Start.java
@@ -0,0 +1,20 @@
+package it.develhope.forloop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println("--------------Starting-----------------");
+
+        List<Integer> fibonacciNumbers = new ArrayList<Integer>();
+        fibonacciNumbers.addAll(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34));
+        for (Integer fibonacciNumber: fibonacciNumbers) {
+            System.out.println(fibonacciNumber);}
+
+        System.out.println("---------------------------------------");
+
+    }
+}
