dc9a183be6e0be21b90c48e7c306c94b0e8b1b8b
Alessio Limina <alessio.limina@gmail.com>
2023-02-09 17:31:20 +0100

    Exercise For Loop 02 Fixed

--- src/it/develhope/forloop/Start.java
+++ src/it/develhope/forloop/Start.java
@@ -1,20 +1,39 @@
 package it.develhope.forloop;
 
-
+/**
+ * This is a Tester class
+ * @author alessiolimina
+ */
 public class Start {
     public static void main(String[] args) {
 
-        System.out.println("--------------Starting-----------------");
+        System.out.println("--------------Printing Fibonacci Numbers-----------------");
 
-        int sum = 0;
-        int nextNum = 1;
-        int previousNumber = 0;
+        /**
+         * Declaring two variables:
+         * firstNumber is the first number
+         * secondNumber is the second number
+         */
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        /**
+         * Creating a For Loop
+         * Printing the firstNumber
+         * The new variable nextNumber is the sum between firstNumber and secondNumber
+         * Assigning to firstNumber the value of the secondNumber
+         * Assigning to secondNumber the value of the nextNumber
+         * Repeat until the end of loop
+         */
 
         for (int i = 1; i<=10; i++){
-            System.out.print(previousNumber + " ");
-            sum = previousNumber + nextNum;
-            previousNumber = nextNum;
-            nextNum = sum;
+            System.out.println(firstNumber);
+            int nextNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+
         }
+        System.out.println("---------------------------------------------------------");
     }
 }
